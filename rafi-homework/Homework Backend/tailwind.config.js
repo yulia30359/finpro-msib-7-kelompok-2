0aca5633118f532b7b8522b9a07927c8771f32b2
Carverra <carreraraverra@gmail.com>
2024-10-27 18:02:00 +0700

    mengupload homework backend - homework week 7

---
+++ rafi-homework/Homework Backend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5C1A1B",
+        secondary: "#FFF1D0",
+        tertiary: "#5ADBFF",
+      }
+    },
+  },
+  plugins: [],
+}
+
